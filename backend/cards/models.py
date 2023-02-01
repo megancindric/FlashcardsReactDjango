dd81a65eb44bf274e5467483ebd2d9baa1825f82
Megan Cindric <megancindric@gmail.com>
2023-01-31 19:12:50 -0600

    📦 NEW:  Added Cards & CardCollections models & serializers, registered with admin site

--- backend/cards/models.py
+++ backend/cards/models.py
@@ -1,3 +1,8 @@
 from django.db import models
-
+from card_collections.models import CardCollection
 # Create your models here.
+
+class Card(models.Model):
+    word = models.CharField(max_length=255)
+    definition = models.CharField(max_length=255)
+    collection = models.ForeignKey(CardCollection, on_delete=models.CASCADE)

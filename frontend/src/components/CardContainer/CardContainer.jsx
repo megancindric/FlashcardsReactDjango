94e36f3ba035ac3d17016c6f72794d5ee6351888
Megan Cindric <megancindric@gmail.com>
2023-02-02 15:15:38 -0600

    📦 NEW: Created initial component files, installed redux toolkit

---
+++ frontend/src/components/CardContainer/CardContainer.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const CardContainer = () => {
+    return (<div>Card Container!</div>);
+}
+
+export default CardContainer;
---
+++ frontend/src/components/CardViewer/CardViewer.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const CardViewer = () => {
+    return (<div>Card Viewer!</div>);
+}
+
+export default CardViewer;

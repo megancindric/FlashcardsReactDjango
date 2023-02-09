d7845606d8a3a1e7c14432a076c0d53a21ba8d09
Megan Cindric <megancindric@gmail.com>
2023-02-08 19:37:43 -0600

    📦 NEW: Styling of header component, wireframing of location of sidebar & card container components

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
-
+import './header.css'
 const Header = () => {
-    return (<div>Header</div>);
+    return (<div>
+        <h2>FLASH!<small>(ahhhhhhhhh)</small></h2>
+    </div>);
 }
 
 export default Header;
